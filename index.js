5c18e4d91d8a38ca6e3aa03df10d6a60a6ec5e3d
mukoubuchi <pharmakeus@gmail.com>
2024-02-23 20:46:11 +0900

    This is a comment.

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+// Authentication mechanism
+app.use("/customer/auth/*", function auth(req, res, next) {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, "your_secret_key_here", (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      req.user = decoded;
+      next();
+    });
+  });
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
